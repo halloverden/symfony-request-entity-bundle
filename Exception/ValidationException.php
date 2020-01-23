8daed61cf627d705abaf2fb3ccecbce92d183f72
Tom Roar Furunes <tom.roar@halloverden.no>
2020-01-23 14:19:53 +0100

    ADD: ValidationException

--- Exception/ValidationException.php
+++ Exception/ValidationException.php
@@ -3,6 +3,10 @@
 
 namespace HalloVerden\RequestEntityBundle\Exception;
 
+
+use HalloVerden\HttpExceptions\BadRequestException;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationInterface;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
@@ -10,31 +14,40 @@ use Symfony\Component\Validator\ConstraintViolationListInterface;
  *
  * @package HalloVerden\RequestEntityBundle\Exception
  */
-class ValidationException extends \Exception {
-  const ERROR_VALIDATION_FAILED = 'VALIDATION_FAILED';
-
-  /**
-   * @var ConstraintViolationListInterface
-   */
-  private $violations;
+class ValidationException extends BadRequestException {
 
   /**
    * ValidationException constructor.
    *
-   * @param ConstraintViolationListInterface $violations
-   * @param int                              $code
-   * @param \Throwable|null                  $previous
+   * @param ConstraintViolationListInterface $violationList
+   * @param string $message
+   * @param \Exception|null $previous
+   * @param int $code
    */
-  public function __construct(ConstraintViolationListInterface $violations, $code = 0, \Throwable $previous = null) {
-    parent::__construct(self::ERROR_VALIDATION_FAILED, $code, $previous);
-    $this->violations = $violations;
+  public function __construct(ConstraintViolationListInterface $violationList, $message = "VALIDATION_ERROR", \Exception $previous = null, $code = 0) {
+    $v = [];
+
+    foreach ($violationList as $violation) {
+      /* @var $violation ConstraintViolationInterface*/
+      $d = new ValidationExceptionViolation($violation->getInvalidValue(), $violation->getMessage(), $violation->getPropertyPath());
+
+      if ($violation instanceof ConstraintViolation) {
+        $d->setErrorName($violation->getConstraint()::getErrorName($violation->getCode()));
+      }
+
+      $v[] = $d->toArray();
+    }
+
+    parent::__construct($message, [
+      "violations" => $v
+    ], $previous, $code);
   }
 
   /**
-   * @return ConstraintViolationListInterface
+   * @return string
    */
-  public function getViolations(): ConstraintViolationListInterface {
-    return $this->violations;
+  public function __toString() {
+    return __CLASS__ . ": [{$this->code}]: {$this->message} -> " . json_encode($this->data) . ".";
   }
 
 }
